28d297739038a6a7d91085e9840fa38123c65b3d
Damian X <dx4336969@gmail.com>
2024-09-05 22:20:03 +0000

    implementing responsive homepage

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -2,7 +2,7 @@ import Handles from "./Handles"
 
 function Footer() {
     return (
-        <div className="bg-[#2D2E32] py-24 ">
+        <div className="bg-[#2D2E32] py-24 md:px-10">
             <div className="max-w-6xl mx-auto text-lg md:grid md:grid-cols-2 justify-between items-center ">
 
             <p className="text-white font-semibold italic ">
@@ -12,7 +12,7 @@ function Footer() {
 
                 <Handles type='footer'/>
                 <div className="flex items-center justify-end pt-4 ">
-                    <p className="text-white font-mono antialiased font-normal">
+                    <p className="text-white font-mono antialiased font-normal ">
                         Damian X
                         </p>
                 </div>
--- src/ui/Hero.jsx
+++ src/ui/Hero.jsx
@@ -7,15 +7,20 @@ function Hero() {
   return (
     <div className="bg-[#F9F9F9] h-dvh  md:px-6 ">
 
-    <div className="md:grid lg:grid-cols-[2fr_1fr] md:grid-cols-[1fr_1fr]  flex-col-reverse flex items-center gap-8 max-w-6xl mx-auto pt-28 relative z-0  ">
+    <div className="md:grid lg:grid-cols-[2fr_1fr] md:grid-cols-[1fr_1fr]  flex-col-reverse flex items-center gap-8 md:max-w-6xl mx-auto md:pt-28 relative z-0 
+     ">
+
       <div className="space-y-7">
         <h1 className="md:text-4xl
-        lg:text-7xl antialiased text-zinc-700 sm:text-xl text-center md:text-left  font-bold mt-4 md:w-80 lg:w-full 
+        lg:text-7xl antialiased text-zinc-700 sm:text-center  sm:text-3xl text-2xl  text-center md:text-left  font-bold  md:w-80 lg:w-full mt-8
          ">
 
-          Front-End React Developer <span className="text-red-400 sm:text-yellow-300 md:text-blue-200">X</span><img src="./images/waving.png" className="w-[4rem] inline" alt="" />
+          <span>
+             Front-End React <br /> Developer
+              <span className="text-red-400 sm:text-yellow-300 md:text-blue-200">X</span><img src="./images/waving.png" className="md:w-[4rem] w-10 inline" alt="" />
+            </span>
         </h1>
-        <p className=" w-[40rem] lg:text-xl md:text-lg font-[Mulish] text-slate-600 ">
+        <p className=" w-[40rem] lg:text-xl md:text-lg font-[Mulish] text-base items-center sm:text-center text-slate-600 md:pl-1 ">
           Hi, I'm Damian X. A passionate Front-end React <br /> Developer based in
           Tamale, Ghana. 📍
         </p>
@@ -26,7 +31,7 @@ function Hero() {
         <img
           src="./images/damian-bg-7.png"
           alt="could not find image"
-          className="w-96  rounded-full scale-110  md:self-start "
+          className="md:w-96 w-64 rounded-full scale-125 md:scale-110  md:self-start "
           />
       </div>
           </div>
