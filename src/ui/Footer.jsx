b080093b8a201a70a145d51dbff24ddcc4c77f61
Damian X <dx4336969@gmail.com>
2024-09-05 22:45:21 +0000

    implementing homepage responsive feature

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -2,17 +2,17 @@ import Handles from "./Handles"
 
 function Footer() {
     return (
-        <div className="bg-[#2D2E32] py-24 md:px-10">
-            <div className="max-w-6xl mx-auto text-lg md:grid md:grid-cols-2 justify-between items-center ">
+        <div className="bg-[#2D2E32] py-24 md:px-10 px-2">
+            <div className="max-w-6xl mx-auto text-lg grid md:grid-cols-2 grid-cols-[2fr_1.5fr] justify-between items-center ">
 
-            <p className="text-white font-semibold italic ">
+            <p className="text-white font-semibold italic text-xs md:text-base ">
             Copyright © 2024. All rights are reserved
             </p>
-            <div className="flex justify-end  gap-10 ">
+            <div className="flex justify-end  gap-4 md:gap-10 px-6 ">
 
                 <Handles type='footer'/>
                 <div className="flex items-center justify-end pt-4 ">
-                    <p className="text-white font-mono antialiased font-normal ">
+                    <p className="text-white font-mono antialiased font-normal text-xs md:text-base">
                         Damian X
                         </p>
                 </div>
--- src/ui/Handles.jsx
+++ src/ui/Handles.jsx
@@ -4,13 +4,13 @@ import { NavLink } from "react-router-dom"
 
 function Handles({type}) {
     return (
-        <div className="space-x-4 items-center flex mt-4">
+        <div className="space-x-4 items-center flex mt-4 ">
             <NavLink to='https://www.linkedin.com/in/sophian-abdul-rahman-909125230/'>
 
-            <IoLogoLinkedin className='hover:fill-secondary-purple transition-all duration-200' size={type? 46:36} color={`${type?'#fff':'#3F3F46'}`}/>
+            <IoLogoLinkedin className={`hover:fill-secondary-purple transition-all duration-200 ${type&& 'size-8'}`} size={type? 46:36} color={`${type?'#fff':'#3F3F46'}`}/>
             </NavLink>
             <NavLink to='https://github.com/Sophianx234' >
-            <AiOutlineGithub className='hover:fill-secondary-purple transition-all duration-200' size={type? 46:36} color={`${type?'#fff':'#3F3F46'}`}/>
+            <AiOutlineGithub className={`hover:fill-secondary-purple transition-all  duration-200   ${type&& 'size-8'}`} size={type? 46:36} color={`${type?'#fff':'#3F3F46'}`}/>
 
             </NavLink>
         </div>
