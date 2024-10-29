cbfb9824813e8485ce6267c7d701dfedb8eb5277
Damian X <dx4336969@gmail.com>
2024-10-29 19:21:05 +0000

    implementing progress section

--- src/ui/Progress.jsx
+++ src/ui/Progress.jsx
@@ -1,9 +1,9 @@
 import ProgressBar from "@ramonak/react-progress-bar";
 function Progress() {
     return (
-        <div className="md:max-w-2xl mx-auto pb-32  ">
+        <div className="md:max-w-2xl mx-auto pb-32 overflow-hidden  ">
             <h1 className="text-center text-xl text-slate-700 font-semibold dark:text-white">Progress</h1>
-            <div className="space-y-3 px-14 py-4 pb-7 mt-5 rounded-lg shadow-md  bg-[#F9F9F9]">
+            <div className="space-y-3 px-14 py-4 pb-7 mt-5 rounded-lg shadow-md mx-9 md:mx-0 bg-[#F9F9F9]">
 
             <div>
                 <h3 className="font-semibold text-slate-700 pl-4">HTML</h3>
